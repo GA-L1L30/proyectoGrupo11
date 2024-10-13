4e0b4bde7b2c03c95aa99c77b45d9e88f21932d0
GonzaSosin <gonzalososin@gmail.com>
2024-10-13 18:47:20 -0300

    se creo detalle y producto y se modifico cosas de la topbar y navbar

--- app/src/main/java/com/example/appgrupo11/MainActivity.kt
+++ app/src/main/java/com/example/appgrupo11/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -54,9 +55,11 @@ class MainActivity : ComponentActivity() {
                     },
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    Column(modifier = Modifier
-                        .padding(innerPadding)
-                        .background(Color.White)) {
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .background(Color.White)
+                    ) {
                         when (selectedNavigationItemIndex) {
                             0 -> HomeScreen()
                             1 -> FindProductsScreen()
@@ -66,48 +69,19 @@ class MainActivity : ComponentActivity() {
 
                         }
 
-                if (showSplashScreen) {
-                    SplashScreen()  // Pantalla de bienvenida
-                } else {
-                    when (selectedNavigationItemIndex) {
-                      0 -> HomeScreen()
-                      1 -> FindProductsScreen()
-                      2 -> OfferAcceptedScreen()
-                      else -> HomeScreen()
+                        if (showSplashScreen) {
+                            SplashScreen()  // Pantalla de bienvenida
+                        } else {
+                            when (selectedNavigationItemIndex) {
+                                0 -> HomeScreen()
+                                1 -> FindProductsScreen()
+                                2 -> OfferAcceptedScreen()
+                                else -> HomeScreen()
+                            }
+                        }
                     }
                 }
-
-                //ProductDetailScreen()
             }
         }
     }
-
-    @Composable
-    fun MainContent(
-        selectedNavigationItemIndex: Int,
-        onNavigationItemSelected: (Int) -> Unit
-    ) {
-        Scaffold(
-            topBar = {
-                CustomTopBar(title = getNavigationList()[selectedNavigationItemIndex].title)
-            },
-            bottomBar = {
-                CustomNavigationBar(
-                    selectedNavigationItem = selectedNavigationItemIndex,
-                    onNavigationItemSelected = onNavigationItemSelected
-                )
-            },
-            modifier = Modifier.fillMaxSize()
-        ) { innerPadding ->
-            Log.d(innerPadding.toString(), "innerPadding")
-
-            when (selectedNavigationItemIndex) {
-                0 -> HomeScreen()
-                1 -> FindProductsScreen()
-                2 -> OfferAcceptedScreen()
-                else -> HomeScreen()
-            }
-        }
-    }
-
 }
